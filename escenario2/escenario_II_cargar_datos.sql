56a650d3a485f4784e04c4cc630e3e5dc3a83dba
monfero <monfero@gmail.com>
2018-12-01 23:52:45 +0100

    Escenario 2

---
+++ escenario2/escenario_II_cargar_datos.sql
@@ -0,0 +1,5 @@
+INSERT into persoa (id_persoa,nome,accions,euros) values (0,'Paco',100,1000);
+INSERT into persoa (id_persoa,nome,accions,euros) values (1,'Paquita',5,100000);
+INSERT into persoa (id_persoa,nome,accions,euros) values (2,'Carlitos',1000,0);
+INSERT into persoa (id_persoa,nome,accions,euros) values (3,'Espe',50,100);
+INSERT into persoa (id_persoa,nome,accions,euros) values (4,'Laila',1000,1000);
---
+++ escenario2/escenario_II_crear_BD.sql
@@ -0,0 +1,14 @@
+CREATE TABLE persoa (
+id_persoa INT, 
+nome VARCHAR(30),
+accions INT,
+euros INT
+);
+
+CREATE TABLE operacions (
+vendedor VARCHAR(30),
+comprador VARCHAR(30),  
+accions VARCHAR(50)
+);
+
+
